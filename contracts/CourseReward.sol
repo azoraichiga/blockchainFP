76525405e841a9cac8342d07a2a40d8ac262cdcf
Brigitta Lucia <blohutabarat@gmail.com>
2026-06-19 15:08:04 +0700

    Add function to get the count of claimers

--- contracts/CourseReward.sol
+++ contracts/CourseReward.sol
@@ -151,6 +151,14 @@ contract CourseReward {
         return address(this).balance;
     }
 
+    function getClaimersCount()
+    external
+    view
+    returns (uint256)
+{
+    return claimers.length;
+}
+
     function getAllClaimers()
         external
         view
